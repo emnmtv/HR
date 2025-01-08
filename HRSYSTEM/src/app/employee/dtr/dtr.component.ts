b7d02e4e3dda99d90284c03215649597febc7002
emnmtv <90402959+emnmtv@users.noreply.github.com>
2025-01-08 12:32:53 +0800

    Revert "1"
    This reverts commit 16b022d7b49055c4d9946dd7c0c040d408b82658.

--- HRSYSTEM/src/app/employee/dtr/dtr.component.ts
+++ HRSYSTEM/src/app/employee/dtr/dtr.component.ts
@@ -18,24 +18,14 @@ export class DtrComponent implements OnInit {
     setInterval(() => this.updateClock(), 1000);
     this.loadRecords();
     this.ensureTodayRecord();
-   
     
   }
  
   updateClock() {
     const now = new Date();
-    this.currentTime = this.convertToPhilippineTimezone(now).toLocaleTimeString();
+    this.currentTime = now.toLocaleTimeString();
     this.currentDate = this.formatDate(now);
-  }
-
-  // Converts the time to Philippine Time Zone (UTC +8)
-  convertToPhilippineTimezone(date: Date): Date {
-    const philippineOffset = 8 * 60; // Philippine Standard Time (PST) is UTC +8
-    const utcDate = new Date(date.toUTCString());
-    utcDate.setMinutes(utcDate.getMinutes() + philippineOffset);
-    return utcDate;
-  }
-
+  } 
 
   ensureTodayRecord() {
     const today = this.formatDate(new Date());
@@ -116,7 +106,6 @@ export class DtrComponent implements OnInit {
   ngAfterViewInit() {
     this.initChart();
   }
-  
 
   calculateTimes(record: any) {
     const [start, end] = record.schedule.split(' - ').map(this.parseTime);
